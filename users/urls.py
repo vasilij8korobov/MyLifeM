46a4da1d17fa71f49291ca37e76d3921c44273a6
vasilij korobov <kavyabass51@gmail.com>
2025-07-23 03:09:50 +0700

    базовая версия(0.1): настройка урлов(завтра продолжу, спать надо)

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CustomLoginView, CustomLogoutView, register
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+]
