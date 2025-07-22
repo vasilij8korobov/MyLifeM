f4673d86a5563b9c083b06f451d0f8d48b8f5529
vasilij korobov <kavyabass51@gmail.com>
2025-07-23 02:58:41 +0700

    базовая версия(0.1)

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def home(request):
+    if request.user.is_authenticated:
+        context = {}
+
+        if hasattr(request.user, 'school_diary'):
+            context['school_diary'] = request.user.school_diary
+        return render(request, 'diary/home.html', context)
+    return redirect('login')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from diary.models import SchoolDiary
+from .forms import SignUpForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+
+            # Создаем школьный дневник, если пользователь младше 18 И выбрал опцию
+            if user.is_minor and form.cleaned_data['wants_school_diary']:
+                SchoolDiary.objects.create(user=user)  # Создаем пустую запись
+
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'registration/register.html', {'form': form})
