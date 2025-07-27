af01bb524063a3ed9041c24e20b062dfd8c93bd8
vasilij korobov <kavyabass51@gmail.com>
2025-07-28 03:12:07 +0700

    Правки проекта(тесты)

--- diary/mixins.py
+++ diary/mixins.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class OwnerRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
     raise_exception = True
+
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
 
